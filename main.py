0e154bf5607a50f1be8bf1c355d10ac25c02411b
saanchoo <s.sancho@alumnos.upm.es>
2025-02-25 19:36:15 +0100

    Añadido .gitignore para ignorar .env

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("GPTrainer")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Acceder a las variables
+api_key = os.getenv("API_KEY")
+# secret_key = os.getenv("SECRET_KEY")
+
+print(f"API Key: {api_key}")
